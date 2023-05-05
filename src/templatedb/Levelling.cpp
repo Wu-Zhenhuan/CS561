fd289e973bf46117618c35b59adbd01915e87880
greesh13 <greeshmashekhar.y@gmail.com>
2023-05-05 16:05:40 -0400

    documentation

--- src/templatedb/Levelling.cpp
+++ src/templatedb/Levelling.cpp
@@ -3,42 +3,67 @@
 //
 
 #include "Levelling.h"
-#include <iostream>
-
-Levelling::Levelling() : currentLevel(0) {}
-
-templatedb::Value Levelling::get(int key) {
+// Get the value of a given key
+        templatedb::Value Levelling::get(int key) {
+    // Iterate through all levels from top to bottom
     for (int i = this->currentLevel - 1; i >= 0; i--) {
+        // Check if the key falls within the min/max range of the current level
         if ((this->mins.at(i) > key) || this->maxs.at(i) < key) {
-            continue;
+            continue; // If not, skip to the next level
         }
+        bool mayContainKey = false;
+        // Check if any of the bloom filters at this level may contain the key
+        for (auto& bf : this->bloomFilters) {
+            if (bf.query(std::to_string(key))) {
+                mayContainKey = true;
+                break;
+            }
+        }
+        if (!mayContainKey) {
+            continue; // If none of the bloom filters may contain the key, skip to the next level
+        }
+        // Check each key-value pair in the current level, and return the value if the key matches
         for (auto &pair: this->levels.at(i)) {
             if (pair.first == key) {
                 return pair.second;
             } else if ((!pair.second.visible) && (pair.first <= key) && (key <= pair.first + pair.second.range)) {
-                return templatedb::Value(false);
+                return templatedb::Value(false); // If the key is in a range tombstone, return false
             }
 
         }
     }
-    return NULL;
+    return NULL; // If the key is not found in any level, return NULL
 }
 
 
+// This function calculates the capacity of a level given its level number.
+// It uses a left shift operation to multiply BUFFER_SIZE by 2^l.
 int Levelling::levelCapacity(int l) {
     return BUFFER_SIZE << l;
 }
 
+// This function flushes a buffer into the levelling data structure.
+// It merges the buffer with the top level, and rebalances the levels if necessary.
+// It also creates a bloom filter for the new run, and adds it to the vector of bloom filters.
 void Levelling::flushIn(std::tuple<run, int, int> buffer) {
+    // Get the buffer run from the input tuple.
     run bufferRun = std::get<0>(buffer);
+
+    // If the levels vector is empty, create a new level and add the buffer run to it.
     if (this->levels.size() == 0) {
         this->newLevel();
         this->levels.push_back(std::move(bufferRun));
         this->mins.push_back(std::get<1>(buffer));
         this->maxs.push_back(std::get<2>(buffer));
-    } else {
+    }
+        // If the levels vector is not empty, merge the buffer with the top level,
+        // and rebalance the levels if necessary.
+    else {
+        // Merge the buffer with the top level.
         run mergedRun = std::move(this->merge(bufferRun, this->levels.at(0)));
         this->levels.at(0) = std::move(mergedRun);
+
+        // Rebalance the levels.
         for (int i = 0; i < this->currentLevel - 1; i++) {
             if (levels.at(i).size() > levelCapacity(levels.size())) {
                 mergedRun = std::move(this->merge(this->levels.at(i), this->levels.at(i+1)));
@@ -56,87 +81,65 @@ void Levelling::flushIn(std::tuple<run, int, int> buffer) {
         }
     }
 
-    // create a bloom filter for the new run
-//    BF::BloomFilter bf(bufferRun.size(), 10);
-//
-//    // insert keys from the new run into the bloom filter
-//    for (auto& pair: bufferRun) {
-//        bf.program(std::to_string(pair.first));
-//    }
-//
-//    // add the bloom filter to the vector of bloom filters
-//    this->bloomFilters.push_back(bf);
-//
-//    // add the new run to the current level
-//    this->levels.at(this->currentLevel - 1) = this->merge(bufferRun, this->levels.at(this->currentLevel - 1));
+    // Create a bloom filter for the new run.
+    BF::BloomFilter bf(bufferRun.size(), 10);
+
+    // Insert keys from the new run into the bloom filter.
+    for (auto& pair: bufferRun) {
+        bf.program(std::to_string(pair.first));
+    }
+
+    // Add the bloom filter to the vector of bloom filters.
+    this->bloomFilters.push_back(bf);
+
+    // Add the new run to the current level.
+    this->levels.at(this->currentLevel - 1) = this->merge(bufferRun, this->levels.at(this->currentLevel - 1));
 }
-//void Levelling::flushIn(std::tuple<run, int, int> buffer) {
-//    run bufferRun = std::get<0>(buffer);
-//    if (this->levels.size()==0) {
-//        this->newLevel();
-//        this->levels.push_back(bufferRun);
-//        this->mins.push_back(std::get<1>(buffer));
-//        this->maxs.push_back(std::get<2>(buffer));
-//    }
-//    else if (levels.at(levels.size()-1).size() > levelCapacity(levels.size())) {
-//        this->currentLevel++;
-//    }
-//    // create a bloom filter for the new run
-//    BF::BloomFilter bf(100, 10);
-////
-////    // insert keys from the new run into the bloom filter
-////    for (auto& pair: bufferRun) {
-////        bf.program(std::to_string(pair.first));
-////    }
-////
-////    // add the bloom filter to the vector of bloom filters
-////    this->bloomFilters.push_back(bf);
-//
-//    // add the new run to the current level
-//    this->levels.at(this->currentLevel - 1) = this->merge(bufferRun, this->levels.at(this->currentLevel - 1));
-//}
 
-
+// This function creates a new level.
+// It creates a vector of Pair objects with the capacity calculated by levelCapacity,
+// and adds it to the levels vector.
 void Levelling::newLevel() {
     std::vector<templatedb::Pair> level(this->levelCapacity(this->currentLevel));
     this->levels.push_back(level);
     this->currentLevel++;
 }
 
+
+
 /**
- * A lovely merge that deal with duplicate in higher way that keeps higher one
- * @param higher
- * @param lower
- * @return
+ * A merge function that deals with duplicate elements by keeping the higher one
+ * @param higher The run with higher keys
+ * @param lower The run with lower keys
+ * @return A merged run with duplicates resolved
  */
 run Levelling::merge(run higher, run lower) {
 
-    /*
-    run resultSet(higher.size() + lower.size());
-    std::merge(higher.begin(), higher.end(), lower.begin(), lower.end(), std::back_inserter(resultSet), [ ](const Pair& lhs, const Pair& rhs )
-    {
-        return lhs.first < rhs.first;
-    });
-    return resultSet;
-    */
+    // Initialize variables
     size_t i = 0;
     size_t j = 0;
     run resultSet;
     resultSet.reserve(higher.size() + lower.size());
+
+    // Merge the two runs, resolving duplicates
     while (i < higher.size() && j < lower.size()) {
         if (higher.at(i).first == lower.at(j).first) {
+            // Keep the higher of the two
             resultSet.push_back(higher.at(i++));
             if (lower.at(j).second.visible || lower.at(j).second.range <= 0) {
-                // Not a range tombstone
+                // If it's not a range tombstone, move on to the next element in lower
                 j++;
             }
         } else if (higher.at(i).first < lower.at(j).first) {
+            // Add the element from higher to the result set
             resultSet.push_back(higher.at(i++));
         } else {
+            // Add the element from lower to the result set
             resultSet.push_back(lower.at(j++));
         }
     }
 
+    // Add any remaining elements to the result set
     if (i < higher.size()) {
         resultSet.insert(resultSet.end(), higher.begin() + i, higher.end());
     } else if (j < lower.size()) {
@@ -145,23 +148,33 @@ run Levelling::merge(run higher, run lower) {
     return std::move(resultSet);
 }
 
+/**
+ * Scans the levelling data structure for all key-value pairs with keys between min_key and max_key (inclusive)
+ * @param min_key The minimum key to scan for
+ * @param max_key The maximum key to scan for
+ * @return A vector of key-value pairs that match the scan criteria
+ */
 std::vector<Pair> Levelling::scan(int min_key, int max_key) {
-    //std::cout<<"************************************"<<std::endl;
     std::vector<Pair> resultSet;
-    // std::cout<<min_key<<", "<<max_key<<std::endl;
     for (int i = this->currentLevel - 1; i >= 0; i--) {
         if ((this->mins.at(i) > max_key) || this->maxs.at(i) < min_key) {
+            // Skip levels that don't contain any keys in the scan range
             continue;
         }
         std::vector<Pair> levelResult;
         for (auto &pair: this->levels.at(i)) {
             levelResult.push_back(pair);
-
         }
+        // Merge the results from the current level with the overall result set
         this->merge(resultSet, levelResult);
     }
     return std::move(resultSet);
 }
+
+/**
+ * Scans the entire levelling data structure for all key-value pairs
+ * @return A vector of all key-value pairs in the data structure
+ */
 std::vector<Pair> Levelling::scan() {
     std::vector<Pair> resultSet;
     for (int i = this->currentLevel - 1; i >= 0; i--) {
@@ -169,8 +182,8 @@ std::vector<Pair> Levelling::scan() {
         for (auto &pair: this->levels.at(i)) {
             levelResult.push_back(pair);
         }
+        // Merge the results from the current level with the overall result set
         this->merge(resultSet, levelResult);
     }
     return std::move(resultSet);
-
 }
--- src/templatedb/Levelling.h
+++ src/templatedb/Levelling.h
@@ -1,36 +1,40 @@
-//
-// Created by Zhenhuan Wu on 4/25/23.
-//
 
+// Header guard to prevent multiple inclusion of the same header
 #ifndef TEMPLATEDB_LEVELLING_H
 #define TEMPLATEDB_LEVELLING_H
 
 #include "Value.h"
 #include "BloomFilter/BloomFilter.h"
-//typedef templatedb::Value Value;
+
+// Alias for templatedb::Pair and templatedb::run
 typedef templatedb::Pair Pair;
 typedef templatedb::run run;
 
+// Class definition for Levelling
 class Levelling {
 private:
-    int currentLevel;
-    std::vector<run> levels;
-    std::vector<BF::BloomFilter> bloomFilters;
-    std::vector<int> mins, maxs;
+    int currentLevel; // The current level of the levelling data structure
+    std::vector<run> levels; // Vector of runs stored in each level of the data structure
+    std::vector<BF::BloomFilter> bloomFilters;// Vector of bloom filters for each run in the data structure
+    std::vector<int> mins, maxs; // Minimum and maximum keys stored in each run
 
 public:
-    //explicit Levelling(int currentLevel);
+// Constructor
     explicit Levelling();
-    templatedb::Value get(int key);
-    void put(int key, templatedb::Value val);
-    std::vector<Pair> scan();
-    std::vector<Pair> scan(int min_key, int max_key);
-    void del(int key);
-    void del(int min_key, int max_key);
-    void flushIn(std::tuple<run, int, int> buffer);
-    int levelCapacity(int l);
-    run merge(run higher, run lower);
-    void newLevel();
+    // Methods for accessing and modifying data in the levelling data structure
+    templatedb::Value get(int key);           // Get the value associated with a given key
+    void put(int key, templatedb::Value val); // Insert or update a key-value pair in the data structure
+    std::vector<Pair> scan();                // Scan the entire data structure and return all key-value pairs
+    std::vector<Pair> scan(int min_key, int max_key); // Scan a specific range of keys and return matching key-value pairs
+    void del(int key);                       // Delete the key-value pair associated with a given key
+    void del(int min_key, int max_key);       // Delete all key-value pairs in a given key range
+    void flushIn(std::tuple<run, int, int> buffer); // Flush a buffer of key-value pairs to the data structure
+
+// Utility methods for managing the levels and runs in the levelling data structure
+    int levelCapacity(int l);                 // Compute the capacity of a level based on its level number
+    run merge(run higher, run lower);         // Merge two runs, resolving conflicts by keeping the higher key-value pair
+    void newLevel();                          // Add a new level to the levelling data structure
+
 };
 
 
